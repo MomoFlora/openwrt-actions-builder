b910b9ed3a2827c3df78b39449a932d4aa7e13f3
MomoFlora <2519840456@qq.com>
2026-04-06 15:25:32 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -103,8 +103,10 @@ sed -i "s/192.168.1.1/10.0.0.1/g" package/base-files/files/bin/config_generate
 # 设置默认主机名
 sed -i "s/hostname='.*'/hostname='ZeroWrt'/g" package/base-files/files/bin/config_generate
 
-# 默认 WIFI 名称
+# 默认 WIFI 名称 && 密码
 sed -i "s/\(set \${si}\.ssid='\${defaults?.ssid || \"\)[^\"]*\(\"}'\)/\1ZeroWrt\2/" package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
+sed -i "s#^set \${si}\.encryption=.*#set \${si}.encryption='psk2'#" package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
+sed -i "s#^set \${si}\.key=.*#set \${si}.key='12345678'#" package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
 
 # 设置默认密码
 default_password=$(openssl passwd -5 password)
