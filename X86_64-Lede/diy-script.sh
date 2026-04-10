7bedcb109b0a40429abcf180c6e2576ae3ca6665
MomoFlora <2519840456@qq.com>
2026-04-10 21:02:51 +0800

    Update diy-script.sh

--- X86_64-Lede/diy-script.sh
+++ X86_64-Lede/diy-script.sh
@@ -68,11 +68,7 @@ git clone --depth=1 https://github.com/Openwrt-Passwall/openwrt-passwall2 packag
 git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
 
 # 主题
-git clone --depth=1 -b 18.06 https://github.com/kiddin9/luci-theme-edge package/new/luci-theme-edge
-git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/new/luci-theme-argon
-git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-app-argon-config package/new/luci-app-argon-config
-git clone --depth=1 https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/new/luci-theme-infinityfreedom
-git_sparse_clone main https://github.com/haiibo/packages luci-theme-atmaterial luci-theme-opentomcat luci-theme-netgear
+git clone --depth=1 -b openwrt-25.12 https://github.com/ZeroWrt/luci-theme-argon package/new/luci-theme-argon
 
 # 在线用户
 git_sparse_clone main https://github.com/haiibo/packages luci-app-onliner
